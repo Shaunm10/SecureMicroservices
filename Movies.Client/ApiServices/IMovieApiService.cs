a9e9e0c1194eca26f02f80471c51437d33b856eb
Shaun Sargent <shaunm10@gmail.com>
2022-03-23 15:42:12 -0400

    Added DTO model

---
+++ Movies.Client/ApiServices/IMovieApiService.cs
@@ -0,0 +1,17 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public interface IMovieApiService
+    {
+        Task<IEnumerable<Movie>> GetMoviesAsync();
+
+        Task<Movie> GetMovieAsync(string id);
+
+        Task<Movie> CreateMovie(Movie movie);
+
+        Task<Movie> UpdateMovie(Movie movie);
+
+        Task DeleteMovie(int id);
+    }
+}
