fc95dc72c20d97f2502b5c3d87761ea708cb0ef8
Shaun Sargent <shaunm10@gmail.com>
2022-03-24 12:33:13 -0400

    Added IDS4 UI

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllersWithViews();
+
 // adds Identity Server's DI services.
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
@@ -14,8 +16,22 @@ builder.Services.AddIdentityServer()
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
 
 // add it to the middleware
 app.UseIdentityServer();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
 app.Run();
