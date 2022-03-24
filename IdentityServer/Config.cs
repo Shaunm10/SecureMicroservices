34299250e8342f4180c036db568f5079b8744cbb
Shaunm10 <shaunm10@gmail.com>
2022-03-24 13:49:47 -0400

    Open id connect (#5)
    * Added IDS4 UI
    
    * - Added Test user and resources to IDS4

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,4 +1,8 @@
-﻿using IdentityServer4.Models;
+﻿using System.Security.Claims;
+using System.Security.Cryptography;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 
 namespace IdentityServer;
@@ -22,6 +26,32 @@ public class Config
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = {MovieApiName}
+                },
+                new Client
+                {
+                    ClientId = "movies_mvc_client",         // must be unique
+                    ClientName = "Movies MVC Web App",      // a description of whom the client is.
+                    AllowedGrantTypes = GrantTypes.Code,    // basically which flow we are using to get the token
+                    AllowRememberConsent = false,           //
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:5002/signin-oidc" // this is the client app port
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = new List<Secret>
+                    {
+                        // used for client verification the client is whom they say they are.
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        // the scopes this client is allowed to have.
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
         }
@@ -29,12 +59,29 @@ public class Config
 
     public static IEnumerable<ApiResource> ApiResources => new List<ApiResource>();
 
-    public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>();
+    public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
+    };
 
     public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>
     {
         new ApiScope(MovieApiName, "Movie API")
     };
 
-    public static List<TestUser> TestUsers => new List<TestUser>();
+    public static List<TestUser> TestUsers => new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "2d4ad762-e232-4538-9bd2-a063cab7186d",
+            Username = "player1",
+            Password = "player1",
+            Claims = new List<Claim>
+            {
+                new Claim(JwtClaimTypes.GivenName,"Player"),
+                new Claim(JwtClaimTypes.FamilyName,"One")
+            }
+        }
+    };
 }
--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,21 +1,36 @@
 using IdentityServer;
-using IdentityServer4.Models;
-using IdentityServer4.Test;
-using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 
 // adds Identity Server's DI services.
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
     .AddInMemoryApiScopes(Config.ApiScopes)
-    //.AddTestUsers(Config.TestUsers)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
 
 // add it to the middleware
 app.UseIdentityServer();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
+
 app.Run();
