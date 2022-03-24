ef100843e947ef4c118757d99538f8a89e52e5f2
Shaun Sargent <shaunm10@gmail.com>
2022-03-24 13:49:06 -0400

    - Added Test user and resources to IDS4

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,7 +1,5 @@
 using IdentityServer;
-using IdentityServer4.Models;
-using IdentityServer4.Test;
-using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +9,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddIdentityServer()
     .AddInMemoryClients(Config.Clients)
     .AddInMemoryApiScopes(Config.ApiScopes)
-    //.AddTestUsers(Config.TestUsers)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddTestUsers(Config.TestUsers)
     .AddDeveloperSigningCredential();
 
 var app = builder.Build();
