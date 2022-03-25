7aa3337c59ca45a112f6ed159b1ee5fbb409730f
Shaun Sargent <shaunm10@gmail.com>
2022-03-25 17:31:49 -0400

    - Added SwaggerUI proxy

--- Movies.Client/ApiServices/MovieApiService.cs
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -9,52 +9,37 @@ namespace Movies.Client.ApiServices
     public class MovieApiService : IMovieApiService
     {
         private readonly ServiceUrls _serviceUrlsConfiguration;
-        private readonly OpenIdConnect _openIdConnectConfiguration;
-
         private readonly IHttpClientFactory _httpClientFactory;
 
         public MovieApiService(
-            IOptionsSnapshot<OpenIdConnect> openIdConnectSnapshot, 
             IOptionsSnapshot<ServiceUrls> serviceUrlsOptionsSnapshot,
             IHttpClientFactory httpClientFactory)
         {
             this._httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
             this._serviceUrlsConfiguration = serviceUrlsOptionsSnapshot?.Value ?? throw new ArgumentNullException(nameof(serviceUrlsOptionsSnapshot));
-            this._openIdConnectConfiguration = openIdConnectSnapshot?.Value ?? throw new ArgumentNullException(nameof(openIdConnectSnapshot));
         }
 
         public async Task<IEnumerable<Movie>> GetMoviesAsync()
         {
-
             var httpClient = this._httpClientFactory.CreateClient("MovieAPIClient");
 
-            var request = new HttpRequestMessage(HttpMethod.Get, "/api/Movies");
+            var movieApi = new ServiceReferences.MoviesApi(this._serviceUrlsConfiguration.MovieApi, httpClient);
 
-            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
-                .ConfigureAwait(false);
+            var serviceMovies = await movieApi.GetMoviesAsync();
 
-            response.EnsureSuccessStatusCode();
+            var test = serviceMovies.Select(m => new Movie
+            {
+                Title = m.Title,
+                Genre = m.Genre,
+                ReleaseDate = DateTime.Parse(m.ReleaseDate.ToString()),
+                ImageUrl = m.ImageUrl,
+                Owner = m.Owner,
+                Id = m.Id,
+                Rating = m.Rating
+            });
 
-            var content = await response.Content.ReadAsStringAsync();
-            var movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
-            return movieList;
-
-            //var httpClient = 
-
-            // get code_access token
-            //var tokenResponse = await this.GetTokenResponseAsync();
-
-            // make request to endpoint
-            //var apiClient = new HttpClient();
-            //apiClient.SetBearerToken(tokenResponse.AccessToken);
-            //var apiResponse = await apiClient.GetAsync($"{this._serviceUrlsConfiguration.MovieApi}/api/movies");
-            //apiResponse.EnsureSuccessStatusCode();
-
-            //var content = await apiResponse.Content.ReadAsStringAsync();
-
-            //// deserialize the response.
-            //List<Movie> movieList = JsonConvert.DeserializeObject<List<Movie>>(content);
-            //return movieList;
+            return test.ToList();
+           
         }
 
         public Task<Movie?> GetMovieAsync(string id)
@@ -77,36 +62,36 @@ namespace Movies.Client.ApiServices
             throw new NotImplementedException();
         }
 
-        private async Task<TokenResponse> GetTokenResponseAsync()
-        {
-            var apiClientCredentials = new ClientCredentialsTokenRequest
-            {
-                Address = $"{this._openIdConnectConfiguration.Authority}/connect/token",
-                ClientId = "movieClient", //this._openIdConnectConfiguration.ClientId,
-                ClientSecret = this._openIdConnectConfiguration.ClientSecret,
+        //private async Task<TokenResponse> GetTokenResponseAsync()
+        //{
+        //    var apiClientCredentials = new ClientCredentialsTokenRequest
+        //    {
+        //        Address = $"{this._openIdConnectConfiguration.Authority}/connect/token",
+        //        ClientId = "movieClient", //this._openIdConnectConfiguration.ClientId,
+        //        ClientSecret = this._openIdConnectConfiguration.ClientSecret,
 
-                // this is the scope our Protected Api requires.
-                Scope = this._openIdConnectConfiguration.MovieApiScope
-            };
+        //        // this is the scope our Protected Api requires.
+        //        Scope = this._openIdConnectConfiguration.MovieApiScope
+        //    };
 
-            // create a new HttpClient to talk to our IdentityServer
-            // TODO: pull from httpClientFactory
-            var client = new HttpClient();
+        //    // create a new HttpClient to talk to our IdentityServer
+        //    // TODO: pull from httpClientFactory
+        //    var client = new HttpClient();
 
-            var disco = await client.GetDiscoveryDocumentAsync(this._openIdConnectConfiguration.Authority);
-            if (disco.IsError)
-            {
-                throw new ApplicationException($"Unable to retrieve discovery document: {disco.Error}");
-            }
+        //    var disco = await client.GetDiscoveryDocumentAsync(this._openIdConnectConfiguration.Authority);
+        //    if (disco.IsError)
+        //    {
+        //        throw new ApplicationException($"Unable to retrieve discovery document: {disco.Error}");
+        //    }
 
-            // authenticate and get an access token from Identity Server
-            var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
-            if (tokenResponse.IsError)
-            {
-                throw new ApplicationException($"Unable to retrieve credential response: {tokenResponse.Error}");
-            }
+        //    // authenticate and get an access token from Identity Server
+        //    var tokenResponse = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+        //    if (tokenResponse.IsError)
+        //    {
+        //        throw new ApplicationException($"Unable to retrieve credential response: {tokenResponse.Error}");
+        //    }
 
-            return tokenResponse;
-        }
+        //    return tokenResponse;
+        //}
     }
 }
