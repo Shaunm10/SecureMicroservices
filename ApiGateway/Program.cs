aa647829d5e33a00037eef132f7f6f3b06ec8014
Shaun Sargent <shaunm10@gmail.com>
2022-04-05 14:41:10 -0400

    - added ocelot services and middleware.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,13 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-var environmentName = builder.Environment.EnvironmentName;
+var builder = WebApplication.CreateBuilder(args);
 
 // pull the config for the current environment.
 builder.Configuration.AddJsonFile($"ocelot.json", true, true);
 
+builder.Services.AddOcelot();
 
+var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+// finally use the Ocelot gateway
+await app.UseOcelot();
+//app.MapGet("/", () => "Ocelot Gatway");
 
 app.Run();
