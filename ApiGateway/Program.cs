0a270ca941248159a215972c47817ed3f1eacc46
Shaun Sargent <shaunm10@gmail.com>
2022-04-08 13:08:31 -0400

    - Added Authoriztion to IPD from Api gateway

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -5,6 +6,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // pull the config for the current environment.
 builder.Configuration.AddJsonFile($"ocelot.json", true, true);
+
+// TODO: pull from configuration 
+var authenticationProviderKey = "IdentityApiKey";
+
+builder.Services.AddAuthentication().AddJwtBearer(authenticationProviderKey, x =>
+{
+    var authorityUrl = builder.Configuration.GetSection("OpenIdConnect")["AuthorityUrl"];
+
+    // add to configuration
+    x.Authority = authorityUrl; // IdentityServerUrl
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddOcelot();
 
